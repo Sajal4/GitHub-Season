22e2fc7af693a1b9fefd62269cf1af3503e51e34
Sajal Debnath <94454173+Sajal4@users.noreply.github.com>
2022-07-01 19:48:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get("/", function(req,res){
 
 app.get("/api/main", function (req, res) {
     res.write("<h1>Express.JS</h1>")
-    reset.write("<p>Express is a light weigtht web application framork to help organize your web application into in mvc architecture on server side.</p>")
+    res.write("<p>Express is a light weigtht web application framork to help organize your web application into in mvc architecture on server side.</p>")
     res.end()
 });
 
