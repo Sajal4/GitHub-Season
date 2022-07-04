f26ccfcf43576bef3547f34eb95655622fbb68ad
Sajal4 <sajaldebnath923@gmail.com>
2022-07-04 19:49:50 +0530

    middleware data add

--- app.js
+++ app.js
@@ -3,10 +3,17 @@
 const express = require("express");
 const app =express();
 
+
+// Middleware: Middleware are like any general functions which takes in three parameters as request, response and next function.
+
+
 const middleware1= (req, res, next)=>{
     console.log("Middleware 1");
     next();
 }
+
+// next() function is very important, after the end of each middleware we should execute next function so that the program execution can move either to next middleware or to main route, if this function is not added then execution will not move forward.
+
 
 app.get("/", middleware1,(req, res)=>{
     res.send("Main page with middleware1")
